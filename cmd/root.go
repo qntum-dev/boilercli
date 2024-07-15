036c1557dca6b4e9b1f00006989884e64c9b99e1
qntum-dev <pritammondal.dev@gmail.com>
2024-07-15 12:04:23 +0530

    some changes

--- cmd/root.go
+++ cmd/root.go
@@ -13,13 +13,8 @@ import (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "boilercli",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Generating docker boilerplates for different frameworks/languages",
+	Long:  `This tool which can be used for generating boilerplates for different frameworks/languages with suitable docker files and docker compose files for development.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
